4cb51d77e522e749fe73dde334542e3eee0cec19
w4570 <35764481+w4570@users.noreply.github.com>
2022-10-13 14:28:20 +0800

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+	HOMEWORK: PHYSICS ENGINE 1: BULLET & FIREWORK PHYSICS
+	MEMBERS: Macario Laurel
+			 Junma Ogawa
+			 Julian Urrutia
+*/
+
+//-------------------- INCLUDES --------------------\\
+
+#include <stdio.h>
+
+//-------------------- MAIN CODE --------------------\\
+
+int main() {
+
+	return 0;
+
+}
